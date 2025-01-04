dc96f79ae52049d7e8ac1d7e44f824ab2fc13d65
Alex Mch <54686138+mclitos@users.noreply.github.com>
2025-01-04 12:03:55 -0600

    Translated into Spanish

--- fullinstall.sh
+++ fullinstall.sh
@@ -8,49 +8,49 @@ apt-get upgrade -y
 apt-get install -y curl
 
 # Ask if the user wants to install sudo
-read -p "Do you want to install sudo? (y/n) " install_sudo
-if [[ $install_sudo =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar con sudo? (s/n)  " install_sudo
+if [[ $install_sudo =~ ^[Ss]$ ]]; then
     apt-get install -y sudo
 fi
 
 # Ask if the user wants to install Docker
-read -p "Do you want to install Docker? (y/n) " install_docker
-if [[ $install_docker =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar Docker? (s/n) " install_docker
+if [[ $install_docker =~ ^[Ss]$ ]]; then
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
 fi
 
 # Ask if the user wants to install Docker Compose
-read -p "Do you want to install Docker Compose? (y/n) " install_compose
-if [[ $install_compose =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar Docker Compose? (s/n) " install_compose
+if [[ $install_compose =~ ^[Ss]$ ]]; then
     curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
 # Ask if the user wants to install Portainer
-read -p "Do you want to install Portainer? (y/n) " install_portainer
-if [[ $install_portainer =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar Portainer? (s/n) " install_portainer
+if [[ $install_portainer =~ ^[Ss]$ ]]; then
     docker volume create portainer_data
     docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 fi
 
 # Ask if the user wants to create a new sudo user
-read -p "Do you want to create a new sudo user? (y/n) " create_user
-if [[ $create_user =~ ^[Yy]$ ]]; then
-    read -p "Enter the desired username: " username
+read -p "¿Quieres crear un nuevo usuario sudo? (s/n) " create_user
+if [[ $create_user =~ ^[Ss]$ ]]; then
+    read -p "Introduzca el nombre de usuario: " username
     while true; do
-        read -s -p "Enter the password: " password
+        read -s -p "Introduzca el  password: " password
         echo
-        read -s -p "Confirm the password: " password_confirm
+        read -s -p "Confirme el password: " password_confirm
         echo
         if [[ $password == $password_confirm ]]; then
             break
         else
-            echo "Passwords do not match. Please try again."
+            echo "El Password no coinciden. Intentalo de nuevo."
         fi
     done
     useradd -m -s /bin/bash $username
     echo "$username:$password" | sudo chpasswd
     usermod -aG sudo,docker $username
-    echo "User $username has been created and added to sudo and docker groups."
+    echo "Se ha creado el usuario $username y se ha agregado a los grupos sudo y docker."
 fi
