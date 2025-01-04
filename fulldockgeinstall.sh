ee7a6013c3fce40e4c57a858877140c04d3984df
Alex Mch <54686138+mclitos@users.noreply.github.com>
2025-01-04 11:58:31 -0600

    Translated into Spanish

--- fulldockgeinstall.sh
+++ fulldockgeinstall.sh
@@ -8,28 +8,28 @@ apt-get upgrade -y
 apt-get install -y curl
 
 # Ask if the user wants to install sudo
-read -p "Do you want to install sudo? (y/n) " install_sudo
-if [[ $install_sudo =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar con sudo? (s/n) " install_sudo
+if [[ $install_sudo =~ ^[Ss]$ ]]; then
     apt-get install -y sudo
 fi
 
 # Ask if the user wants to install Docker
-read -p "Do you want to install Docker? (y/n) " install_docker
-if [[ $install_docker =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar Docker? (s/n) " install_docker
+if [[ $install_docker =~ ^[Ss]$ ]]; then
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
 fi
 
 # Ask if the user wants to install Docker Compose
-read -p "Do you want to install Docker Compose? (y/n) " install_compose
-if [[ $install_compose =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar Docker Compose? (s/n) " install_compose
+if [[ $install_compose =~ ^[Ss]$ ]]; then
     curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
 # Ask if the user wants to install Dockge
-read -p "Do you want to install Dockge? (y/n) " install_dockge
-if [[ $install_dockge =~ ^[Yy]$ ]]; then
+read -p "¿Quieres instalar Dockge? (s/n) " install_dockge
+if [[ $install_dockge =~ ^[Ss]$ ]]; then
     # Create directories that store your stacks and stores Dockge's stack
     mkdir -p /opt/stacks /opt/dockge
     cd /opt/dockge
@@ -43,8 +43,8 @@ if [[ $install_dockge =~ ^[Yy]$ ]]; then
 fi
 
 # Ask if the user wants to create a new sudo user
-read -p "Do you want to create a new sudo user? (y/n) " create_user
-if [[ $create_user =~ ^[Yy]$ ]]; then
+read -p "¿Quieres crear un nuevo usuario sudo? (s/n) " create_user
+if [[ $create_user =~ ^[Ss]$ ]]; then
     read -p "Enter the desired username: " username
     while true; do
         read -s -p "Enter the password: " password
@@ -54,11 +54,11 @@ if [[ $create_user =~ ^[Yy]$ ]]; then
         if [[ $password == $password_confirm ]]; then
             break
         else
-            echo "Passwords do not match. Please try again."
+            echo "El Passwords no coinciden. Intentalo de nuevo."
         fi
     done
     useradd -m -s /bin/bash $username
     echo "$username:$password" | sudo chpasswd
     usermod -aG sudo,docker $username
-    echo "User $username has been created and added to sudo and docker groups."
+    echo "Se ha creado el usuario $username y se ha agregado a los grupos sudo y docker."
 fi
